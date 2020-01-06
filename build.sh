34fa74eba61c136f17f6fb6c01158fefe5ef505d
Blafy <alban.boye@protonmail.com>
2020-01-06 19:25:32 +0100

    fix ci/cd

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 mkdir raspbian-root
 
-sudo debootstrap --arch armhf --variant minbase --no-check-gpg --foreign buster raspbian-root http://archive.raspbian.org/raspbian
+debootstrap --arch armhf --variant minbase --no-check-gpg --foreign buster raspbian-root http://archive.raspbian.org/raspbian
 
-sudo tar -C raspbian-root -c . | docker import - raspbian-temp
+tar -C raspbian-root -c . | docker import - raspbian-temp
 
 #docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
-docker build -t raspbian .
+docker build -t blafy/raspbian .
