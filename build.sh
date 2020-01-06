7875588ad24badd8efdb6bffce392d1078c4a786
Blafy <alban.boye@protonmail.com>
2020-01-06 19:27:14 +0100

    qemu fix

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ debootstrap --arch armhf --variant minbase --no-check-gpg --foreign buster raspb
 
 tar -C raspbian-root -c . | docker import - raspbian-temp
 
-#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 docker build -t blafy/raspbian .
